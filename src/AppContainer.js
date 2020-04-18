a386a43f44d0bd4bacf36287d604424e23b8f4a6
alexandermagee <alexandermagee1@gmail.com>
2020-04-18 13:35:01 +0100

    notes added on updateImage

--- src/AppContainer.js
+++ src/AppContainer.js
@@ -6,22 +6,26 @@ export class AppContainer extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            image1: "https://desenio.co.uk/bilder/artiklar/zoom/11329_1.jpg",
+            /*image1: "https://desenio.co.uk/bilder/artiklar/zoom/11329_1.jpg",
             image2: "https://www.cityam.com/wp-content/uploads/2019/11/Centre-Point-view-2019-1280x720.jpg",
-            image3: "https://www.pictorem.com/collection/900_Melanie-Viola_18-00521.jpg",
-            images: ["one","Goose","googa"]
+            image3: "https://www.pictorem.com/collection/900_Melanie-Viola_18-00521.jpg",*/
+            images: ["https://desenio.co.uk/bilder/artiklar/zoom/11329_1.jpg","https://www.cityam.com/wp-content/uploads/2019/11/Centre-Point-view-2019-1280x720.jpg","https://www.pictorem.com/collection/900_Melanie-Viola_18-00521.jpg"]
         }
     }
 
     updateImage = (newURL,imageNumber) => {
         let newImage = newURL;
+        /* Creating a clone of the images array stops splice mutating state directlly */
         let imageArray = this.state.images;
+        /* imageNumber comes from the id of specific input form that triggered function,
+          1 refers to the number of items within array to delete (update with new image),
+          newURL is the image given to the input form by the user */
         imageArray.splice(imageNumber,1,newURL);
+        /* the whole array is updated on the state as updating individual items caused issues */
         this.setState({
             /*[imageNumber] : newImage, */
             images : imageArray 
         });
-        console.log(imageArray)
     }
 
     render(){
